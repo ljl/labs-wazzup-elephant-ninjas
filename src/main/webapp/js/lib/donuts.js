04403457902177653bd7740e4c3f41a127b28e37
Lasse Jul-Larsen <ljl@enonic.com>
2013-03-08 14:55:50 +0100

    Added donut and stuffs

---
+++ src/main/webapp/js/lib/donuts.js
@@ -0,0 +1,19 @@
+$(function () {
+  "use strict";
+  var colors = ['green', 'yellow', 'red']
+    , segmentSize = 100.0 / colors.length;
+  colors.push(colors[colors.length - 1]);
+  $('.donut-arrow').each(function () {
+    var $this = $(this)
+      , percentage = +$this.data('percentage')
+      , color;
+
+    if (percentage > 100)
+      percentage = 100;
+    else if (percentage < 0)
+      percentage = 0;
+
+    $this.css('transform', 'rotate(' + ((1.8 * percentage) - 90) + 'deg)');
+    $this.parent().addClass(colors[Math.floor(percentage / segmentSize)]);
+  });
+});

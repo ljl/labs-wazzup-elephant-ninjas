92674ece47d78dc6a0c96dde550e5595a5166eb4
Jørund Vier Skriubakken <jvs@enonic.com>
2013-03-22 15:21:09 +0100

    Updated BranchBarComponent

--- src/main/webapp/js/view/BranchBarComponent.js
+++ src/main/webapp/js/view/BranchBarComponent.js
@@ -14,36 +14,50 @@ ui.branchBar = function () {
                 var gravatarBase = "http://www.gravatar.com/avatar/";
                 var html = '<div class="bar">' +
                            '<div class="head">';
-                html += '<div class="status_' + branchBar.status + '">';
-                html += branchBar.name +
-                        '</div>';
                 html += '<ul class="tests">';
+                html += '<li class="status_' + branchBar.status + '">';
+                html += branchBar.name + '</li>';
+
                 if (branchBar.failedTestCount > 0) {
-                    html += '<li class="failed">' + branchBar.failedTestCount + '</li>';
-                    html += '<li class="passed">' + branchBar.passedTestCount + '</li>';
-                    html += '<li class="ignored">' + branchBar.ignoredTestCount + '</li>';
+                    html +=
+                    '<li class="failed">' + branchBar.failedTestCount + ' failed ' + ', ' + branchBar.passedTestCount + ' passed</li>';
+                    html += '<li class="ignored">' + branchBar.ignoredTestCount + ' ignored</li>';
                 }
                 else {
-                    html += '<li class="passed">' + branchBar.passedTestCount + '</li>';
-                    html += '<li class="ignored">' + branchBar.ignoredTestCount + '</li>';
+                    html += '<li class="passed">' + branchBar.passedTestCount + ' passed</li>';
+                    html += '<li class="ignored">' + branchBar.ignoredTestCount + ' ignored</li>';
                 }
 
                 if (branchBar.codeCoverage != undefined) {
                     html += '<li class="codeCoverage">' + branchBar.codeCoverage + '</li>';
                 }
+                else {
+                    html += '<li class="codeCoverage">?</li>';
+                }
+                html += '<li class="changers">';
+                html += '<img width="56" height="56" src="' + gravatarBase + gravatarHash + '"/>';
+                html += '</li>';
+
+                html += '<li class="changeMessages">';
+                html += '<span class="message">' + branchBar.changes[0].message;
+                html += '</span>';
+                html += '</li>';
+
                 html += '</ul>' +
-                        '</div>' +
-                        '<ul class="changes">' +
-                        '<li>' +
-                        '<img src="' + gravatarBase + gravatarHash + '"/>' +
-                        '<span class="message">' +
-                        branchBar.changes[0].message +
-                        '</span>' +
-                        '<span class="author">' +
-                        '</span>' +
-                        '</li>' +
-                        '</ul>' +
                         '</div>';
+
+                /*html += '<ul class="changes">' +
+                 '<li>' +
+                 '<img src="' + gravatarBase + gravatarHash + '"/>' +
+                 '<span class="message">' +
+                 branchBar.changes[0].message +
+                 '</span>' +
+                 '<span class="author">' +
+                 '</span>' +
+                 '</li>' +
+                 '</ul>';*/
+
+                html += '</div>';
 
                 $(branchBarsEl).append(html);
             });
